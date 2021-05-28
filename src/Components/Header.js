185136353782a6bc55ade5d40dc78b8655b7df03
Ivan Reinaldo <reinaldoivan777@gmail.com>
2021-05-28 07:59:21 +0700

    done header

---
+++ src/Components/Header.js
@@ -0,0 +1,129 @@
+import React from "react";
+import styled from "styled-components";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+
+const Container = styled.div`
+  height: 60px;
+  background-color: #0f1111;
+  display: flex;
+  align-items: center;
+  color: #ffffff;
+  justify-content: space-between;
+  padding: 0 11px;
+`;
+
+const HeaderLogo = styled.div`
+  display: flex;
+  align-items: center;
+  img {
+    width: 100px;
+  }
+`;
+
+const HeaderOptionAddress = styled.div`
+  padding: 0 9px;
+  display: flex;
+  align-items: center;
+`;
+
+const OptionLineOne = styled.div``;
+
+const OptionLineTwo = styled.div`
+  font-weight: 700;
+`;
+
+const HeaderSearch = styled.div`
+  display: flex;
+  flex-grow: 1;
+  height: 40px;
+  border-radius: 4px;
+  overflow: hidden;
+  margin: 0 4px;
+  :focus-within {
+    box-shadow: 0 0 0 3px #f90;
+  }
+`;
+
+const HeaderSearchInput = styled.input`
+  flex-grow: 1;
+  border: 0;
+
+  :focus {
+    outline: none;
+  }
+`;
+
+const HeaderSearchIconContainer = styled.div`
+  background-color: #febd69;
+  width: 45px;
+  color: #000000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HeaderNavItems = styled.div`
+  display: flex;
+`;
+
+const HeaderOption = styled.div`
+  padding: 10px 9px;
+`;
+
+const HeaderOptionCart = styled.div`
+  display: flex;
+  align-items: center;
+  padding-right: 9px;
+`;
+
+const CartCount = styled.div`
+  padding-left: 4px;
+`;
+
+function Header() {
+  return (
+    <Container>
+      <HeaderLogo>
+        <img
+          src="https://mikekitko.com/wp-content/uploads/2019/10/amazon-logo-white-768x232.png"
+          alt=""
+        />
+      </HeaderLogo>
+      <HeaderOptionAddress>
+        <LocationOnIcon />
+        <HeaderOption>
+          <OptionLineOne>Hello</OptionLineOne>
+          <OptionLineTwo>Select your address</OptionLineTwo>
+        </HeaderOption>
+      </HeaderOptionAddress>
+
+      <HeaderSearch>
+        <HeaderSearchInput type="text" />
+        <HeaderSearchIconContainer>
+          <SearchIcon />
+        </HeaderSearchIconContainer>
+      </HeaderSearch>
+
+      <HeaderNavItems>
+        <HeaderOption>
+          <OptionLineOne>Hello, Ivan</OptionLineOne>
+          <OptionLineTwo>Account & lists</OptionLineTwo>
+        </HeaderOption>
+
+        <HeaderOption>
+          <OptionLineOne>Returns</OptionLineOne>
+          <OptionLineTwo>& Orders</OptionLineTwo>
+        </HeaderOption>
+
+        <HeaderOptionCart>
+          <ShoppingBasketIcon />
+          <CartCount>5</CartCount>
+        </HeaderOptionCart>
+      </HeaderNavItems>
+    </Container>
+  );
+}
+
+export default Header;
